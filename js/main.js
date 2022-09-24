b5c588f7bcd0f1c11a28529cc9c0ad36c5624073
Paula <paulaalminana@gamil.com>
2022-09-24 11:52:28 +0200

    añadir todas las imagenes con id en index y js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 let precioTotal = document.getElementById("precioTotal");
 let precioFinal = 0;
-precioTotal.innerHTML =`${precioFinal}`;
+precioTotal.innerHTML =`${precioFinal}€`;
 
 let makeup = [
     {
@@ -36,8 +36,178 @@ let makeup = [
         id:"base",
         precio: 37
     },
+
+    {
+        id:"serumdunk",
+        precio: 37
+    },
+    {
+        id:"serumordinary",
+        precio: 37
+    },
+    {
+        id:"biooil",
+        precio: 37
+    },
+    {
+        id:"cerave",
+        precio: 37
+    },
+    {
+        id:"laneig",
+        precio: 37
+    },
+    {
+        id:"contornokiels",
+        precio: 37
+    },
+    {
+        id:"laura",
+        precio: 37
+    },
+    {
+        id:"ordinary2",
+        precio: 37
+    },
+    {
+        id:"creamvichy",
+        precio: 37
+    },
     {
         id:"cremaole",
+        precio: 37
+    },
+    {
+        id:"fenty",
+        precio: 37
+    },
+    {
+        id:"rare",
+        precio: 37
+    },
+    {
+        id:"lauramercie",
+        precio: 37
+    },
+    {
+        id:"narscolorete",
+        precio: 37
+    },
+    {
+   
+        id:"correctortarte",
+        precio: 37
+    },
+    {
+        id:"itcosmetic",
+        precio: 37
+    },
+    {
+        id:"lauramercie",
+        precio: 37
+    },
+    {
+        id:"rare",
+        precio: 37
+    },
+    {
+        id:"chanel",
+        precio: 37
+    },
+    {
+        id:"nars",
+        precio: 37
+    },
+    {
+        id:"benefit",
+        precio: 37
+    },
+    {
+        id:"touch",
+        precio: 37
+    },
+    {
+        id:"ctil",
+        precio: 37
+    },
+    {
+        id:"glosiercolorete",
+        precio: 37
+    },
+    {
+        id:"diorlabial",
+        precio: 37
+    },
+    {
+        id:"hourglass",
+        precio: 37
+    },
+    {
+        id:"glosierbalm",
+        precio: 37
+    },
+    {
+        id:"ctlabial",
+        precio: 37
+    },
+    {
+        id:"tooface",
+        precio: 37
+    },
+    {
+        id:"buxon",
+        precio: 37
+    },
+    {
+        id:"clarins",
+        precio: 37
+    },
+    {
+        id:"fresh",
+        precio: 37
+    },
+    {
+        id:"dyson",
+        precio: 37
+    },
+    {
+        id:"dysonrizador",
+        precio: 37
+    },
+    {
+        id:"hairvitamins",
+        precio: 37
+    },
+    {
+        id:"dysonsecador",
+        precio: 37
+    },
+    {
+        id:"hairvitamins",
+        precio: 37
+    },
+    {
+        id:"rizador",
+        precio: 37
+    },
+    {
+        id:"brocha",
+        precio: 37
+    },
+    {
+        id:"uñasdior",
+        precio: 37
+    },
+    {
+        id:"spray",
+        precio: 37
+    },
+    {
+        id:"urban",
+        precio: 37
+    },
+    {
+        id:"maskcaudalie",
         precio: 37
     }
 ]
@@ -65,6 +235,6 @@ const drop = (ev) => {
     
 
     precioFinal += makeupNuevo.precio;
-    precioTotal.innerHTML =  `${precioFinal} `;
+    precioTotal.innerHTML =  `${precioFinal}€`;
 
 }
